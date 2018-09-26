59aa3516e5108283be840dca1f87ad10ba934899
unknown <awesomeriptide@gmail.com>
2018-09-26 21:08:02 +0800

    fixed weapons

--- lib/weapons.dart
+++ lib/weapons.dart
@@ -1,16 +1,16 @@
 import 'package:dart_case_bot/weapon.dart';
 import 'package:dart_case_bot/weapons/pistols.dart';
-import 'package:dart_case_bot/weapons/shotguns.dart' ;
-import 'package:dart_case_bot/weapons/submachine_guns.dart' ;
-import 'package:dart_case_bot/weapons/rifles.dart' ;
-import 'package:dart_case_bot/weapons/machine_guns.dart' ;
+import 'package:dart_case_bot/weapons/shotguns.dart';
+import 'package:dart_case_bot/weapons/submachine_guns.dart';
+import 'package:dart_case_bot/weapons/rifles.dart';
+import 'package:dart_case_bot/weapons/machine_guns.dart';
 import 'package:dart_case_bot/weapons/knives.dart';
 
 class Weapons {
-static WeaponClass pistols = Pistols();
-static WeaponClass shotguns = Shotguns();
-static WeaponClass submachine_guns = SubmachineGuns();
-static WeaponClass rifles = Rifles();
-static WeaponClass machine_guns = MachineGuns();
-static WeaponClass knives = Knives();
-};
+  static WeaponClass pistols = Pistols();
+  static WeaponClass shotguns = Shotguns();
+  static WeaponClass submachine_guns = SubmachineGuns();
+  static WeaponClass rifles = Rifles();
+  static WeaponClass machine_guns = MachineGuns();
+  static WeaponClass knives = Knives();
+}
